aa7521ffc7e7cbf5008e9ae156999c09362c4c3b
feranaya2001 <53525700+feranaya2001@users.noreply.github.com>
2026-07-20 02:33:29 +0000

    traspaso_card 2.0.1: Se siguen hacendo ajustes, ahora sobre export csv y la tabla de totales superior, toma 2...

--- traspaso_card.php
+++ traspaso_card.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 /* Copyright (C) 2017       Laurent Destailleur     <eldy@users.sourceforge.net>
  * Copyright (C) 2024-2025  Frédéric France         <frederic.france@free.fr>
  * Copyright (C) 2026		Fernando Anaya Alba			<consultor.sistemas@ajigsa.com>
- * Version: 2.0.0
+ * Version: 2.0.1
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
@@ -825,6 +825,23 @@ if ($object->id > 0 && (empty($action) || ($action != 'edit' && $action != 'crea
 		$formconfirm = $form->formconfirm($_SERVER["PHP_SELF"].'?id='.$object->id, 'Validar Traspaso', '¿Está seguro de validar este traspaso? Una vez validado no podrá modificarlo ni regresarlo a borrador.', 'confirm_validate_traspaso', '', 0, 1);
 	}
 
+	//Export CSV
+	if ($action == 'export_csv_lines') {
+				$sql_export = "SELECT p.ref as prod_ref, p.label as prod_label, l.qty, l.pmp, l.amount FROM ".MAIN_DB_PREFIX."traspasomultiempresa_traspasoline as l LEFT JOIN ".MAIN_DB_PREFIX."product as p ON p.rowid = l.fk_product WHERE l.fk_traspaso = ".((int) $object->id)." AND l.fk_product > 0 ORDER BY l.rowid ASC";
+				$res_export = $db->query($sql_export);			
+		if ($res_export) {
+			header('Content-Type: text/csv; charset=UTF-8');
+			header('Content-Disposition: attachment; filename="traspaso_'.dol_sanitizeFileName($object->ref).'.csv"');
+			$output = fopen('php://output', 'w');
+			fputcsv($output, array('Referencia', 'Descripcion', 'Cantidad', 'Costo Promedio', 'Importe'));
+			while ($obj_export = $db->fetch_object($res_export)) {
+				fputcsv($output, array($obj_export->prod_ref, $obj_export->prod_label, $obj_export->qty, $obj_export->pmp, $obj_export->amount));
+			}
+			fclose($output);
+			exit;
+		}
+	}	
+	
 	// Clone confirmation
 	if ($action == 'clone') {
 		// Create an array for form
@@ -915,7 +932,8 @@ if ($object->id > 0 && (empty($action) || ($action != 'edit' && $action != 'crea
 		$total_qty = (float) $obj_resumen->total_qty;
 		$total_amount = (float) $obj_resumen->total_amount;
 	}
-	$morehtmlright = '<table class="nobordernopadding right">';
+	//$morehtmlright = '<table class="nobordernopadding right">';
+	$morehtmlright = '<table class="nobordernopadding right" style="margin-top: 45px;">';
 	$morehtmlright .= '<tr><td class="right paddingright">Total Partidas:</td><td class="right"><strong>'.$nb_partidas.'</strong></td></tr>';
 	$morehtmlright .= '<tr><td class="right paddingright">Total Cantidad:</td><td class="right"><strong>'.$total_qty.'</strong></td></tr>';
 	$morehtmlright .= '<tr><td class="right paddingright">Importe Total:</td><td class="right"><strong>'.price($total_amount, 0, '', 1, -1, -1, $conf->currency).'</strong></td></tr>';
@@ -1264,23 +1282,6 @@ jQuery(document).ready(function() {
 				print dolGetButtonAction('', $langs->trans('ToClone'), 'default', $_SERVER['PHP_SELF'].'?id='.$object->id.(!empty($object->socid) ? '&socid='.$object->socid : '').'&action=clone&token='.newToken(), '', $permissiontoadd);
 			}
 			
-			//Export CSV
-			if ($action == 'export_csv_lines') {
-				$sql_export = "SELECT p.ref as prod_ref, p.label as prod_label, l.qty, l.pmp, l.amount FROM ".MAIN_DB_PREFIX."traspasomultiempresa_traspasoline as l LEFT JOIN ".MAIN_DB_PREFIX."product as p ON p.rowid = l.fk_product WHERE l.fk_traspaso = ".((int) $object->id)." AND l.fk_product > 0 ORDER BY l.rowid ASC";
-				$res_export = $db->query($sql_export);			
-			if ($res_export) {
-				header('Content-Type: text/csv; charset=UTF-8');
-				header('Content-Disposition: attachment; filename="traspaso_'.dol_sanitizeFileName($object->ref).'.csv"');
-				$output = fopen('php://output', 'w');
-				fputcsv($output, array('Referencia', 'Descripcion', 'Cantidad', 'Costo Promedio', 'Importe'));
-				while ($obj_export = $db->fetch_object($res_export)) {
-					fputcsv($output, array($obj_export->prod_ref, $obj_export->prod_label, $obj_export->qty, $obj_export->pmp, $obj_export->amount));
-				}
-				fclose($output);
-				exit;
-				}
-			}	
-
 			// Imprimir Ticket
 			$ticketUrl = DOL_URL_ROOT.'/custom/traspasomultiempresa/tpl/tramul_ticket.tpl.php?id='.$object->id;
 			print dolGetButtonAction('', 'Imprimir Ticket', 'default', $ticketUrl, '', $permissiontoread, array('attr' => array('target' => '_blank')));
